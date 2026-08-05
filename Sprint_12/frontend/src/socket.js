e6f9619570791d7e6cc9a737d2f89d9e6a031936
Daksh Choudhary <dakshchoudhary8881@gmail.com>
2026-08-05 14:10:35 +0530

    Update socket.js

--- Sprint_12/frontend/src/socket.js
+++ Sprint_12/frontend/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const SERVER_URL = 'https://internship-prodesk-it.onrender.com/';
+const SERVER_URL = 'https://sprint-12-backend.vercel.app/';
 
 export function createSocket() {
   return io(SERVER_URL, {
