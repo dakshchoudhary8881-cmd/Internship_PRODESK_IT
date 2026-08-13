830dea84f79e5350a249437b10df1899c08b9de7
Daksh Choudhary <dakshchoudhary8881@gmail.com>
2026-08-13 11:26:22 +0530

    made the code connects with backedn to frontend

--- Sprint_14/server/server.js
+++ Sprint_14/server/server.js
@@ -10,7 +10,11 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT_URL || '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  credentials: true
+}));
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
