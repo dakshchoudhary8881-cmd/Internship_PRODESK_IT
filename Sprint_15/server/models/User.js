62410ae479d00046ae4be16b32d2ecdcd781d486
Daksh Choudhary <dakshchoudhary8881@gmail.com>
2026-08-15 23:37:58 +0530

    fix: remove next from async pre-save hook, Mongoose 7+ compat

--- Sprint_15/server/models/User.js
+++ Sprint_15/server/models/User.js
@@ -24,13 +24,12 @@ const userSchema = new mongoose.Schema(
 );
 
 // Hash password before saving
-userSchema.pre('save', async function (next) {
+userSchema.pre('save', async function () {
   if (!this.isModified('password')) {
-    return next(); // ← add return
+    return; // ← no next()
   }
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
-  next();
 });
 
 const User = mongoose.model('User', userSchema);
